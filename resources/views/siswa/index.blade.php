63c69bb1425a7ea4cc445a01176b5c53efa2dace
Rei091200 <reihannanda2000@gmail.com>
2020-07-19 13:05:01 +0700

    commit on 'master'

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -28,8 +28,8 @@
 										<tbody>
                     @foreach($data_siswa as $siswa)
                     <tr>
-                      <td>{{ $siswa->nama_depan }}</td>
-                      <td>{{ $siswa->nama_belakang }}</td>
+                      <td><a href="/siswa/{{$siswa->id}}/profile">{{ $siswa->nama_depan }}</a></td>
+                      <td><a href="/siswa/{{$siswa->id}}/profile">{{ $siswa->nama_belakang }}</a></td>
                       <td>{{ $siswa->jenis_kelamin }}</td>
                       <td>{{ $siswa->agama }}</td>
                       <td>{{ $siswa->alamat }}</td>
