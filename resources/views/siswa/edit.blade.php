c93d7ce24f7f5b47470d806f662743ac25c2ec00
NR <56571192+Rei091200@users.noreply.github.com>
2020-07-19 12:00:59 +0700

    Update edit.blade.php

--- resources/views/siswa/edit.blade.php
+++ resources/views/siswa/edit.blade.php
@@ -50,7 +50,7 @@
                                     </div>
                                 </form>
                             </div>
-							          </div>
+					    </div>
                   </div>  
               </div>  
           </div>  
