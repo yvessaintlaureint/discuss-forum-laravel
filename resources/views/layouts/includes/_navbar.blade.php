78212496051e6711d64068fbcf003d610b9547e7
Rei091200 <reihannanda2000@gmail.com>
2020-07-19 12:14:27 +0700

    Message ('edit on navbar')

--- resources/views/layouts/includes/_navbar.blade.php
+++ resources/views/layouts/includes/_navbar.blade.php
@@ -6,9 +6,9 @@
 		<div class="navbar-btn">
 			<button type="button" class="btn-toggle-fullwidth"><i class="lnr lnr-arrow-left-circle"></i></button>
 		</div>
-		<form class="navbar-form navbar-left">
+		<form class="navbar-form navbar-left" method="GET" action="/siswa">
 			<div class="input-group">
-				<input type="text" value="" class="form-control" placeholder="Cari data siswa...">
+				<input name="cari" type="text" value="" class="form-control" placeholder="Cari data siswa...">
 				<span class="input-group-btn"><button type="button" class="btn btn-primary">Go</button></span>
 			</div>
 		</form>
