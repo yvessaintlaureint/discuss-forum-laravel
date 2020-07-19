ad20bde99663d3686bd5ff1f266218b173b8d543
Rei091200 <reihannanda2000@gmail.com>
2020-07-19 12:54:33 +0700

    commit on 'master'

--- app/Siswa.php
+++ app/Siswa.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Siswa extends Model
 {
     protected $table = 'siswa';
-    protected $fillable = ['nama_depan', 'nama_belakang', 'jenis_kelamin', 'agama', 'alamat'];
+    protected $fillable = ['nama_depan', 'nama_belakang', 'jenis_kelamin', 'agama', 'alamat', 'avatar'];
 }
